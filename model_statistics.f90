7cf48aa39450eb5ea5f6041f376f8847147e5c70
finn-okeeffe <okeeffefinn@gmail.com>
2023-05-17 15:09:08 +1200

    Cleaned and commented model_statistics.f90

--- model_statistics.f90
+++ model_statistics.f90
@@ -1,3 +1,8 @@
+! model_statistics is an outdated name. Most of the functions in this file got moved to more
+! natural places, now it just holds the function to load data and initialise the code
+! before we know the parameters, and holds some parameters for the grid, integration, and
+! ray tracing.
+
 module model_statistics
 
   use initialize_k
@@ -7,7 +12,7 @@ module model_statistics
 
   implicit none
 
-  !! INTEGRATION OPTIONS
+  !! INTEGRATION AND RAYTRACING PARAMETERS
   integer, parameter :: N=5000 ! number of points in r grid (not including 0)
   ! ! 1000 is too low, significantly reduces accuracy
   integer, parameter :: num_steps = 10000 ! number of points to use in integration of R(t,r)
@@ -17,7 +22,7 @@ module model_statistics
   !           doing integration for finding error in k(r)
   integer, parameter :: MAX_K_ITERS = 100 ! number to max iterations when finding k(r)
 
-  ! local variables
+  !! GRID AND COMPOSITE VARIABLES
   real, allocatable :: r(:) ! holds r grid values
   real, dimension(NUM_CATALOGUES, NUM_Z_POINTS) :: comp_C0, comp_C1, comp_C2
   ! logical :: errored(0:N_PIX-1)
@@ -31,10 +36,15 @@ module model_statistics
   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 contains
   subroutine pre_model_setup(use_saved_multipoles)
+    ! setup grid, load COMPOSITE data, and calculate C_l for the observed COMPOSITE
+    ! catalogue if the comp_Cl.dat file is missing (which stores these values)
+
+    ! arguments
     logical, intent(in) :: use_saved_multipoles
+    ! TRUE: load C_l values for observed COMPOSITE from comp_Cl.dat
+    ! FALSE: recalculate C_l values for observed COMPOSITE, and save to comp_Cl.dat
 
     real :: z
-
 
     ! allocate arrays
     allocate(r(N))
@@ -45,26 +55,33 @@ contains
     allocate(M_array(N))
     allocate(comp_rays(NUM_DATA_POINTS))
 
-    ! initialise values
+    ! initialise grid values
     r = [(500.*i/N, i=1,N)]
     r_array = [0., r]
     max_r = maxval(r)
 
-    ! ! !! load composite sample
+    !! load COMPOSITE observational data (redshift, d_L, (l,b), uncertainties, etc)
     print*,"Loading Composite Data..."
     call load_composite_data()
-    print*,"Performing Dipole Analysis on Composite..."
+
+    !! get C_l values for COMPOSITE
+    print*,"Acquiring Dipole Analysis on Composite..."
+    ! load from file
     if ( use_saved_multipoles ) then
       open(1, file="comp_Cl.dat", status="old")
       do i = 1, NUM_Z_POINTS
         read(1,*) z, comp_C0(:,i), comp_C1(:,i), comp_C2(:,i)
         print*,"z:",z
       end do
+    
+    ! calculate from observed quantities
     else
       print*,"Calculating Multipoles"
       call get_Hubble_Multipoles(cz_arr, comp_C0, comp_C1, comp_C2)
 
       print*, "Saving composite dipole"
+
+      ! save to comp_Cl.dat
       open(1, file="comp_Cl.dat", status='replace')
       do i = 1, NUM_Z_POINTS
         write(1,*) z_points(i), comp_C0(:,i), comp_C1(:,i), comp_C2(:,i)
@@ -73,8 +90,8 @@ contains
       print*,"finished"
     end if
 
+    ! set average C_l variables in trace_composite.f90
     forall (i=1:NUM_Z_POINTS) mean_comp_c1(i) = sum(comp_C1(:,i))/NUM_CATALOGUES
     forall (i=1:NUM_Z_POINTS) mean_comp_c2(i) = sum(comp_C2(:,i))/NUM_CATALOGUES
   end subroutine pre_model_setup
-
 end module model_statistics

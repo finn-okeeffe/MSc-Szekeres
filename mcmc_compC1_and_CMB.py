f00b60a777ba1043281a4bc97ed774ae80700bc5
finn-okeeffe <okeeffefinn@gmail.com>
2023-05-18 11:40:34 +1200

    cleaned and commented last python file

--- mcmc_compC1_and_CMB.py
+++ mcmc_compC1_and_CMB.py
@@ -1,12 +1,14 @@
+## Performs MCMC analysis considering both the CMB and COMPOSITE dipole power fits
+
 import numpy as np
 import szekeres as sz
 from scipy.optimize import minimize
 from szekeres_prob_funcs import *
 import emcee
 
-# print(sz.__doc__)
 
 def log_prob(theta):
+    ## returns the log posterior probability
     # theta = [delta_0, alpha, r_0, r_obs, theta_obs]
     x = theta_to_x(theta)
     params = x[:3]
@@ -29,7 +31,7 @@ def log_prob(theta):
     if CMB_quad > OBS_CMB_D2:
         CMB_quad_nlp = (CMB_quad - OBS_CMB_D2)**2 / (sigma**2)
 
-    
+    # COMPOSITE nll
     dips, quads = sz.hubble_multipoles(coords)
     vec = np.mat(dips - C12_means[:num_z])
     C_nlp = 0.5*vec*C1_Cov_inv*(vec.T)
@@ -37,41 +39,44 @@ def log_prob(theta):
     return -C_nlp[0,0]/num_z - CMB_D1_nll - CMB_quad_nlp
 
 
-nlp = lambda theta: -log_prob(theta)
+## Code for maximising the probability to find the best fit
+# nlp = lambda theta: -log_prob(theta)
+# theta = np.array([-0.98, 0.8, 38.5, 40.0, 2.677])
+# nlp_theta = nlp(theta)
+# print(f"Initial params: {theta}")
+# print(f"nlp: {nlp_theta}")
 
-theta = np.array([-0.98, 0.8, 38.5, 40.0, 2.677])
-nlp_theta = nlp(theta)
-print(f"Initial params: {theta}")
-print(f"nlp: {nlp_theta}")
+# print("attemping maximising of probability...")
+# res = minimize(nlp, theta, bounds=bounds[:-1])
 
-print("attemping maximising of probability...")
-res = minimize(nlp, theta, bounds=bounds[:-1])
+# print("Success:",res.success)
+# print("solution:",res.x)
+# print("nlp:",res.fun)
+# print("message:",res.message)
 
-print("Success:",res.success)
-print("solution:",res.x)
-print("nlp:",res.fun)
-print("message:",res.message)
+# # log results
+# with open("TestRuns/MinimizeRes.txt", "a") as f:
+#     f.write("============================================")
+#     f.write(f"Initial Theta: {theta}\n")
+#     f.write(f"Initial lp: {nlp_theta}\n")
+#     f.write(f"Success: {res.success}\n")
+#     f.write(f"message: {res.message}\n")
+#     f.write(f"Final Theta: {res.x}\n")
+#     f.write(f"Final lp: {res.fun}\n")
+#     f.write("============================================")
 
-with open("TestRuns/MinimizeRes.txt", "a") as f:
-    f.write("============================================")
-    f.write(f"Initial Theta: {theta}\n")
-    f.write(f"Initial lp: {nlp_theta}\n")
-    f.write(f"Success: {res.success}\n")
-    f.write(f"message: {res.message}\n")
-    f.write(f"Final Theta: {res.x}\n")
-    f.write(f"Final lp: {res.fun}\n")
-    f.write("============================================")
 ####################################################################################
 ##                      INITIAL CONDITIONS
 ####################################################################################
-# print("Setting up initial conditions...")
-# filename = "TestRuns/COMPC1_and_CMB_backend_fixed_params.h5"
-# ndim = 5
-# nwalkers = 32
+print("Setting up initial conditions...")
+ndim = 5
+nwalkers = 32
 
-# backend = emcee.backends.HDFBackend(filename)
+# Backend
+filename = "TestRuns/COMPC1_and_CMB_backend_fixed_params.h5"
+backend = emcee.backends.HDFBackend(filename)
 
-# # New Backend
+# # Generate random initial guess
 # theta_0 = np.zeros((nwalkers,ndim))
 # for i in range(nwalkers):
 #     print("--------------------------------------------------------------")
@@ -83,20 +88,19 @@ with open("TestRuns/MinimizeRes.txt", "a") as f:
 #     if nlp == -np.inf:
 #         raise Exception("Chosen initial parameters have zero probability")
 #     print("--------------------------------------------------------------")
+# # new backend
 # backend.reset(nwalkers, ndim)
 
+# Resume from saved backend
+theta_0 = None
 
+# Initialize the sampler
+print("Initializing the ensemble sampler...")
+sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob, backend=backend)
 
-# # # Resume from saved backend
-# # theta_0 = None
-
-# # Initialize the sampler
-# print("Initializing the ensemble sampler...")
-# sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob, backend=backend)
-
-# ####################################################################################
-# ##                      MCMC
-# ####################################################################################
-# print("Running MCMC...")
-# num_samples = 20000
-# sampler.run_mcmc(theta_0, num_samples, progress=True)
+####################################################################################
+##                      MCMC
+####################################################################################
+print("Running MCMC...")
+num_samples = 20000 # number of samples to generate
+sampler.run_mcmc(theta_0, num_samples, progress=True)

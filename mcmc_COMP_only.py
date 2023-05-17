50da01b203729d0d9cd93c0abca3804e9f09f022
finn-okeeffe <okeeffefinn@gmail.com>
2023-05-18 11:36:53 +1200

    Cleaned and commented mcmc_COMP_only.py

--- mcmc_COMP_only.py
+++ mcmc_COMP_only.py
@@ -1,16 +1,15 @@
+## Performs MCMC analysis only considering the fit to the COMPOISITE catalogue
+
 import numpy as np
 import szekeres as sz
 from scipy.optimize import minimize
 from szekeres_prob_funcs import *
 import emcee
 
-# print(sz.__doc__)
-
 
 def log_prob_fixed_params(theta):
+    ## returns log posterior probability only considering the fit to COMPOSITE
     # theta = [delta_0, alpha, r_0, r_obs, theta_obs]
-
-    
     x = theta_to_x(theta)
 
     if not(satisfies_constraints(x)): return -np.inf
@@ -20,6 +19,7 @@ def log_prob_fixed_params(theta):
     vec = np.mat(np.append(dips, quads) - C12_means)
     lp = -0.5*vec*C12_Cov_inv*(vec.T)
 
+    ## code for only considering the dipole power, not the quadrupole power
     # dips = dips-C12_means[:num_z]
     # dips = np.mat(dips)
     # covinv = np.mat(C12_Cov_inv[:num_z, :num_z]) # dipole part of covariance matrix
@@ -28,9 +28,11 @@ def log_prob_fixed_params(theta):
     return lp[0,0]
 
 
+# test probability function
 theta = np.array([-0.89, 0.69, 38.22, 38.29, 2.39])
 print(f"Params: {theta}")
 print(f"lp: {log_prob_fixed_params(theta)}")
+
 ####################################################################################
 ##                      INITIAL CONDITIONS
 ####################################################################################
@@ -38,8 +40,13 @@ print("Setting up initial conditions...")
 ndim = 5
 nwalkers = 32
 
-# initial guess
-# random initial guesses
+## setup backend (saving progress)
+print("Setting up backend...")
+filename = "TestRuns/COMP_C1_only_backend_fixed_params.h5"
+backend = emcee.backends.HDFBackend(filename)
+
+#### INITIAL GUESS
+## generate random initial guesses
 # theta_0 = np.zeros((nwalkers,ndim))
 # for i in range(nwalkers):
 #     print("--------------------------------------------------------------")
@@ -51,25 +58,19 @@ nwalkers = 32
 #     if nlp == -np.inf:
 #         raise Exception("Chosen initial parameters have zero probability")
 #     print("--------------------------------------------------------------")
-
-# # backend (saving progress)
-# print("Setting up backend...")
-# filename = "TestRuns/COMP_C1_only_backend_fixed_params.h5"
-# backend = emcee.backends.HDFBackend(filename)
-
 # # New Backend
 # backend.reset(nwalkers, ndim)
 
-# # # Resume from saved backend
-# # theta_0 = None
+## Resume from saved backend
+theta_0 = None
 
-# # Initialize the sampler
-# print("Initializing the ensemble sampler...")
-# sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob_fixed_params, backend=backend)
+# Initialize the sampler
+print("Initializing the ensemble sampler...")
+sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob_fixed_params, backend=backend)
 
 ####################################################################################
 ##                      MCMC
 ####################################################################################
-# print("Running MCMC...")
-# num_samples = 20000
-# sampler.run_mcmc(theta_0, num_samples, progress=True)
+print("Running MCMC...")
+num_samples = 20000 # size of catalogue to generate
+sampler.run_mcmc(theta_0, num_samples, progress=True)
